886a35a423d9bc5a395336d1dc2a0ae41c511577
Dale  Chen <dalechen0351@gmail.com>
2019-12-22 22:07:44 +0800

    added houjie stl project

---
+++ BasicCppStudy/houjie_stl/houjie_stl.cpp
@@ -0,0 +1,23 @@
+﻿// houjie_stl.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional> // 仿函数
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+	int ia[6] = { 27,210, 12, 44, 349,69 };
+	vector<int, allocator<int>> vi(ia, ia + 6);// 容器 和 迭代器 分配器
+	cout << std::count_if(vi.begin(), vi.end(), not1(bind2nd(less<int>(), 40)));// 算法 仿函数适配器 *2  仿函数（less）
+
+	for( auto i : vi) // C++11
+	{
+		cout << i << endl;
+	}
+}
+
